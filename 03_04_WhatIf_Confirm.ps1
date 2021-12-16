1e185352d3f5844263c6849cc12c93c8187b949f
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-12-16 11:36:26 +0100

    Add files via upload

---
+++ 03_04_WhatIf_Confirm.ps1
@@ -0,0 +1,13 @@
+Clear-Host
+
+#Is only a simulation
+Start-Service –Name Fax -WhatIf
+
+#Here the execution must be confirmed
+Start-Service –Name Fax -Confirm
+
+#-Confirm is ignored
+Start-Service –Name Fax –WhatIf -Confirm
+
+#Also here
+Start-Service –Name Fax -Confirm -WhatIf
