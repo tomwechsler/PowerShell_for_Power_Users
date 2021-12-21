ece16b007a8911153640d811eafc1b87251a0425
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-12-21 06:30:14 +0100

    Add files via upload

---
+++ 02_05_Help.ps1
@@ -0,0 +1,31 @@
+Clear-Host
+
+Get-Help -Name Get-TimeZone
+
+Get-Help -Name Get-TimeZone -Online
+	
+#https://docs.microsoft.com/de-de/powershell/module/
+#https://aka.ms/pscore6-docs
+
+Update-Help * -Force
+
+Update-Help -UICulture en-US -Force
+
+Get-Command Get-Help -Syntax
+
+Get-Help -Name Get-Process -Examples
+
+Get-Help -Name Get-Process -Full
+
+Get-Help Get-Help
+
+#Help on conceptual topics of PowerShell are to be understood from detailed background texts
+Get-Help -Name about_*
+
+Get-Help -Name about_CommonParameters
+
+#If the search term is found in several help files, the hits are listed
+Get-Help "process"
+
+#The question is in which help files the term Virtual Disk can be found. 
+Get-Help "Virtual Disk"
