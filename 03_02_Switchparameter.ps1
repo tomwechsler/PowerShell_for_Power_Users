814980509685c3357c3372641657dd5d4122aa2d
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-12-24 06:42:27 +0100

    Add files via upload

---
+++ 03_02_Switchparameter.ps1
@@ -0,0 +1,22 @@
+#A switch parameter is an on-off switch as the English term switch already suggests. 
+#It is activated or switched on by the fact that it is used in the first place
+
+Clear-Host
+
+#Gets the items and child items 
+Get-ChildItem
+
+#Let's use the help
+Get-Help Get-ChildItem
+
+#Lets look closer
+Get-Help Get-ChildItem -Parameter Recurse
+
+#The contents of the current directory and all subdirectories are displayed
+Get-ChildItem -Path C:\Windows -Recurse
+
+#Equivalent is
+Get-ChildItem -Path C:\Windows -Recurse:$true
+
+#$false means as if -Recurse had not been used
+Get-ChildItem -Path C:\Windows -Recurse:$false
